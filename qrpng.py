457e9d88afe07417fcdbe844f839e75ba1c71429
ChuJian <chujian_hit@163.com>
2023-11-22 13:15:13 +0800

    Update qrpng.py

--- qrpng.py
+++ qrpng.py
@@ -1,4 +1,4 @@
-import qrcode
+import pyqrcode
 from PIL import Image
 from pyzbar.pyzbar import decode
 
@@ -8,19 +8,14 @@ def read_text_file(file_path):
         content = file.read()
     return content
 
-# 将文本转换为QR码并保存为图片
-def generate_qr_code(text, qr_code_file_path):
-    qr = qrcode.QRCode(
-        version=1,
-        error_correction=qrcode.constants.ERROR_CORRECT_L,
-        box_size=10,
-        border=4,
-    )
-    qr.add_data(text)
-    qr.make(fit=True)
+# 将文本转换为QR码并保存为图片，然后打印图片
+def generate_qr_code_and_print(text):
+    qr = pyqrcode.create(text)
 
-    qr_image = qr.make_image(fill_color="black", back_color="white")
-    qr_image.save(qr_code_file_path)
+    qr.png("qr_code.png", scale=10)
+
+    qr_image = Image.open("qr_code.png")
+    qr_image.show()
 
 # 识别QR码图片并返回内容
 def decode_qr_code(qr_code_file_path):
@@ -35,11 +30,11 @@ def decode_qr_code(qr_code_file_path):
 text_file_path = "path/to/your/text_file.txt"
 text_content = read_text_file(text_file_path)
 
-# 生成QR码并保存为图片
-qr_code_file_path = "path/to/save/qr_code.png"
-generate_qr_code(text_content, qr_code_file_path)
+# 生成QR码并保存为图片，并打印图片
+generate_qr_code_and_print(text_content)
 
-# 识别QR码并打印内容
+# 接下来的解码部分与之前的代码相同，识别QR码并打印内容
+qr_code_file_path = "qr_code.png"
 decoded_content = decode_qr_code(qr_code_file_path)
 if decoded_content:
     print("QR Code Content:", decoded_content)
